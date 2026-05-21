8cea38605d27819da74a37ee61410ce9a0f6127b
Marcos Queiroz <marcosamfq123@gmail.com>
2026-05-21 18:50:07 -0300

    atividade de form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContatoComponent } from './components/contato-component/contato-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContatoComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('form');
+
 }
---
+++ src/app/components/contato-component/contato-component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-contato-component',
+  imports: [FormsModule],
+  templateUrl: './contato-component.html',
+  styleUrl: './contato-component.css',
+})
+export class ContatoComponent {
+  contato = {
+    nome: '',
+    email: '',
+    assunto: '',
+    mensagem: '',
+  }
+
+ onSubmit(form: NgForm) {
+   if (form.valid) {
+     console.log(form.value);
+   }
+ }
+
+}
