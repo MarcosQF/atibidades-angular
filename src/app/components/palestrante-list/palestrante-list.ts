30dfa8d737158f6afd5c99f17694dce83d6b2ec6
Marcos Queiroz <marcosamfq123@gmail.com>
2026-06-26 16:10:55 -0300

    atividade 10

--- src/app/components/palestrante-list/palestrante-list.ts
+++ src/app/components/palestrante-list/palestrante-list.ts
@@ -1,34 +1,64 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { PalestranteService } from '../../services/palestrante-service';
 import { Palestrante } from '../../interfaces/palestrante';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-palestrante-list',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './palestrante-list.html',
   styleUrl: './palestrante-list.css',
 })
-export class PalestranteList implements OnInit{
-private palestranteService = inject(PalestranteService);
+export class PalestranteList implements OnInit {
+  private palestranteService = inject(PalestranteService);
+  private destroyRef = inject(DestroyRef)
 
   palestrantes = signal<Palestrante[]>([]);
   loading = signal<boolean>(true);
+  termoBusca = signal<string>('');
 
   ngOnInit(): void {
     this.carregarPalestrantes();
   }
 
   carregarPalestrantes(): void {
-    this.palestranteService.buscarPalestrantes().subscribe({
+    this.loading.set(true);
+    this.palestranteService.buscarPalestrantes()
+    .pipe(
+      takeUntilDestroyed(this.destroyRef)
+    )
+    .subscribe({
       next: (dados) => {
         this.palestrantes.set(dados);
         this.loading.set(false);
       },
       error: (err) => {
-        console.error('Erro ao buscar palestrantes', err);
+        console.error('Erro ao buscar', err);
         this.loading.set(false);
       }
     });
   }
 
+  filtrarPorEmpresa(): void {
+    const empresa = this.termoBusca().trim();
+
+    if (!empresa) {
+      this.carregarPalestrantes();
+      return;
+    }
+
+    this.loading.set(true);
+    this.palestranteService.buscarPalestrantesEmpresa(empresa).subscribe({
+      next: (dados) => {
+        this.palestrantes.set(dados);
+        this.loading.set(false);
+      },
+      error: (err) => {
+        console.error('Erro ao filtrar', err);
+        this.loading.set(false);
+      }
+    });
+  }
 }
--- src/app/services/palestrante-service.ts
+++ src/app/services/palestrante-service.ts
@@ -1,18 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 import { Palestrante } from '../interfaces/palestrante';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PalestranteService {
-
   private http = inject(HttpClient);
   private apiUrl = 'http://localhost:3001/api/palestrantes';
 
   buscarPalestrantes(): Observable<Palestrante[]> {
-    return this.http.get<Palestrante[]>(this.apiUrl);
+    return this.http.get<Palestrante[]>(this.apiUrl).pipe(
+      tap((palestrantes) => console.log('Total de palestrantes recebidos:', palestrantes.length)),
+      catchError((error) => {
+        console.error('Erro ao buscar palestrantes:', error);
+        return of([]);
+      }),
+    );
   }
 
+  buscarPalestrantesEmpresa(empresa: string) {
+    return this.http.get<Palestrante[]>(this.apiUrl).pipe(
+      map((palestrantes) => palestrantes.filter((palestrante) => palestrante.empresa == empresa)),
+    );
+  }
 }
